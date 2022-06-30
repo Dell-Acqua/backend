a34fa95bd1a514a5eb504e3728954d6f07b56eab
Agustin Citro <dellacquahidroponia@gmail.com>
2022-06-29 20:21:12 -0400

    segundo commit

--- ejercicio2.js
+++ ejercicio2.js
@@ -10,18 +10,47 @@ class Contenedor {
         this.productos.push(producto);
         async function save_producto() {
             try {
-                await fs.promises.appendFile('./productos.txt', JSON.stringify([producto], null, '\t'))
+                await fs.promises.appendFile('./productos.txt', JSON.stringify([producto], null, 2))
                 console.log(producto.id)
-                return productos
+                
             } catch (err) {
-                console.log('error al guardar')
+                console.log('error al guardar',err)
             }
         }
         save_producto()
+         }
+
+    getById(id){
+        const art = this.productos.map(ids => productos.id == id);
+        console.log(art)
     }
 
-    getById(id){}
+    getAll(){
+        async function obtener_datos (){
+            try {
+               const data = await fs.promises.readFile('./productos.txt','utf-8');
+               const datos = JSON.parse(data);
+               console.log(datos);
+            }
+            catch (err) {
+                console.log("Hubo un error en la importacion del objeto", err)
+            }
+        }
+        obtener_datos()
+    }
 
+    deleteAll(){
+        async function borrar_datos (){
+            try {
+                await fs.promises.writeFile('./productos.txt','\n');
+                console.log("Se eliminaron todos los datos")
+            }
+            catch (error){
+                console.log ("Hubo un error al intentar borrar todos los datos",error)
+            }
+        }
+        borrar_datos()
+    }
 }
 
 
@@ -30,18 +59,13 @@ const articulo = new Contenedor; productos = [
     title: 'Lechuga Hidroponica',
     price: 150,
     thumbnail: 'imgLechuga'
-}, {
-    title: 'Rucula Hidroponica',
-    price: 450,
-    thumbnail: 'Rucula Hidroponica'
-}, {
-    title: 'Tomates Cherry',
-    price: 500,
-    thumbnail: 'imgTomates'
 }];
+
 
 for (i in productos) {
     articulo.save(productos[i]);
 }
 
-articulo.getById(2)
+articulo.getById(1);
+articulo.getAll();
+articulo.deleteAll();
